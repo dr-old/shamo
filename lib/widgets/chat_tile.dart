f9c348f0567aeb6da4f145568745ee05d39b7aa8
dr-old <73454862+dr-old@users.noreply.github.com>
2022-12-13 17:15:20 +0700

    UPDATE CHAT DETAIL

--- lib/widgets/chat_tile.dart
+++ lib/widgets/chat_tile.dart
@@ -9,66 +9,74 @@ class ChatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: 33,
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 54,
-                height: 54,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: yellowColor,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/logo_vertical.jpeg',
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          '/chat-detail',
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(
+          top: 33,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 54,
+                  height: 54,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: yellowColor,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/logo_vertical.jpeg',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 12,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Shoe Store',
-                      style: primaryTextStyle.copyWith(
-                        fontSize: 15,
-                      ),
-                    ),
-                    Text(
-                      'Good morning, this item on Good morning, this item on Good morning, this item on',
-                      style: secondaryTextStyle.copyWith(
-                        fontWeight: light,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ],
+                SizedBox(
+                  width: 12,
                 ),
-              ),
-              Text(
-                'Now',
-                style: secondaryTextStyle.copyWith(
-                  fontSize: 10,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Shoe Store',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        'Good morning, this item on Good morning, this item on Good morning, this item on',
+                        style: secondaryTextStyle.copyWith(
+                          fontWeight: light,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 12,
-          ),
-          Divider(
-            thickness: 1,
-            color: backgroundColor2,
-          )
-        ],
+                Text(
+                  'Now',
+                  style: secondaryTextStyle.copyWith(
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Divider(
+              thickness: 1,
+              color: backgroundColor2,
+            )
+          ],
+        ),
       ),
     );
   }
