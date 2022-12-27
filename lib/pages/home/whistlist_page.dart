35c9f5281a52863958e86485fd65862b5f842abb
dr-old <73454862+dr-old@users.noreply.github.com>
2022-12-27 10:43:15 +0700

    UPDATE EMPTY WISHLIST

--- lib/pages/home/whistlist_page.dart
+++ lib/pages/home/whistlist_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:shamo/theme.dart';
@@ -6,13 +7,90 @@ import 'package:shamo/theme.dart';
 class WhishlistPage extends StatelessWidget {
   const WhishlistPage({super.key});
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      centerTitle: true,
+      title: Text(
+        'Favorite Shoes',
+        style: primaryTextStyle.copyWith(
+          fontSize: 18,
+          fontWeight: medium,
+        ),
+      ),
+      elevation: 0,
+    );
+  }
+
+  Widget emptyWishlist() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        color: backgroundColor3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icons8-favorite-96.png',
+              color: secondaryColor,
+              width: 74,
+            ),
+            SizedBox(
+              height: 23,
+            ),
+            Text(
+              'You dont\'t have dream shoes?',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'Let\'s find your favorite shoes',
+              style: secondaryTextStyle,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 44,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 24,
+                  ),
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  'Explore Store',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Whishlist Page',
-        style: primaryTextStyle,
-      ),
+    return Column(
+      children: [
+        header(),
+        emptyWishlist(),
+      ],
     );
   }
 }
