44ab11003672090e478c94dde256268bd64f4c13
dr-old <73454862+dr-old@users.noreply.github.com>
2022-12-28 16:34:17 +0700

    UPDATE EMPTY CART IN CART PAGE

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:shamo/theme.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Your Cart',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+        elevation: 0,
+      );
+    }
+
+    Widget emptyCart() {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icons8-shopping-cart-96.png',
+              width: 80,
+              color: secondaryColor,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Opps! Your Cart is Empty',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'Let\'s find your favorite shoes',
+              style: secondaryTextStyle,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 44,
+              width: 154,
+              child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  )),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: header(),
+      ),
+      body: emptyCart(),
+    );
+  }
+}
